3415ff8362a9a3a01e123e58624cc4b92f709e25
Abdullahi Yunus <45062451+Abdulkbk@users.noreply.github.com>
2021-11-19 00:14:21 +0100

    first commit

---
+++ pages/index.js
@@ -0,0 +1,13 @@
+import Head from 'next/head'
+import Image from 'next/image'
+import styles from '../styles/Home.module.css'
+
+export default function Home() {
+  return (
+    <div className="bg-indigo-300 text-gray-800 text-center h-screen flex justify-center items-center">
+      <>
+        <span className="text-3xl bold">Welcome to Liquidity Finance,</span> <span className="text-gray-600">where magic happens</span>
+      </>
+    </div>
+  )
+}
